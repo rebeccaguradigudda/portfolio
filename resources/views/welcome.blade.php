ebf676bc27b4d6e3a75c369c1963e2d5e7d47da2
Rebecca Guradigudda <rebecca@chakradesign.co>
2020-01-13 17:32:25 +0530

    build website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    rebecca
                 </div>
 
                 <div class="links">
